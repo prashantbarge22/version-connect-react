75a7a4e9a1b1a1faa222581f3c107de6a5bd3570
prashant barge <prashantbarge22@gmail.com>
2021-08-11 09:41:55 +0530

    signup page made

---
+++ src/components/SignUpPage.js
@@ -0,0 +1,22 @@
+import SignUpPageForm from "./SignUpForm";
+import "../css/signuppage.css"
+function SignUpPage(){
+    return(
+        <div className="container-lg">
+            
+            <header>
+
+            </header>
+
+            <div>
+                <SignUpPageForm/> 
+                <div>
+                    <img className="side-image" src="\fashion-online-shopping-sites.jpg" alt="background"></img>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default SignUpPage;
