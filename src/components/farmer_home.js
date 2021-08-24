a62ca9c6bad4ed49c6f1a2632e557ff31ac445e6
prashant barge <prashantbarge22@gmail.com>
2021-08-24 08:44:40 +0530

    cards added

--- src/components/farmer_home.js
+++ src/components/farmer_home.js
@@ -43,6 +43,21 @@ function FarmerHome(){
                 <span class="visually-hidden">Next</span>
             </button>
             </div>
+            <div className="advertisment">
+            <div class="card" style={{width:"20%"}} >
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR9fj2VgrfiDC5LL1QiaqIDYow-a5SdY-z0Q&usqp=CAU" class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            <div class="card" style={{width:"20%"}} >
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR9fj2VgrfiDC5LL1QiaqIDYow-a5SdY-z0Q&usqp=CAU" class="card-img-top" alt="..."/>
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            </div>
+            </div>
+            
            </div>
         </div>
     )
